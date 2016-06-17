7008cac6bc1d35a432bb1858bf241d12699d40c2
OOKBEE10 <OOKBEE10@minhtu-pc.local>
2016-06-17 17:34:48 +0700

    Add File

---
+++ AES256Testing/AES.h
@@ -0,0 +1,15 @@
+//
+//  AES.h
+//  AES256Testing
+//
+//  Created by Thỏ on 6/17/16.
+//  Copyright © 2016 Thỏ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AES : NSObject
+
++(NSString*)decrypt:(NSString*)cipherText withKey:(NSString*)key;
+
+@end
